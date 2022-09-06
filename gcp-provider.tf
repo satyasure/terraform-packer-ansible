81e31d69d869b29012724c2d569f85bff25d82d7
satyasure <51406383+satyasure@users.noreply.github.com>
2022-09-06 23:48:51 +0800

    Update gcp-provider.tf

--- gcp-provider.tf
+++ gcp-provider.tf
@@ -3,3 +3,8 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-c"
 }
+provider "google-beta" {
+  project     = "even-kite-361714"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
